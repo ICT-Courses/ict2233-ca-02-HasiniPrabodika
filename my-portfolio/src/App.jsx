1144ced800e732f9a330f6ce343953b7f4537248
Hasini Prabodhika <ar112724@fhss.sjp.ac.lk>
2025-09-23 01:16:32 +0530

    Add About route to App.jsx routing configuration

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from "./components/Layouts";
 import Home from "./pages/Home";
+import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import './index.css'
 
@@ -11,6 +12,7 @@ function App() {
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
