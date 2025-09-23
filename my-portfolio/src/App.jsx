758ec40d3d5dd0ad7ae391d3fd54f48e7a69da00
Hasini Prabodhika <ar112724@fhss.sjp.ac.lk>
2025-09-23 19:37:57 +0530

    Add route for Contact page

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Skills from "./pages/Skills";
+import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 import './index.css'
 
@@ -17,6 +18,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
