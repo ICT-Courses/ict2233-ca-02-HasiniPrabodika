f47ae3d3c2cf7170ee1650261c9a27c61abb61b6
Hasini Prabodhika <ar112724@fhss.sjp.ac.lk>
2025-09-24 22:50:19 +0530

    Update vite.config.js file

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
- 
+  base: '/ict2233-ca-02-HasiniPrabodika/',
 })
